40aaab15c9d62a25432af8fa30fed39a111b110f
Satya Shreya <109357452+shreyapresingu@users.noreply.github.com>
2022-10-29 11:18:22 +0530

    Update assesment1.js

--- assesment1.js
+++ assesment1.js
@@ -1,7 +1,13 @@
-var obj = { "a":1,"b":2,"tesla":12,"tats":112 }; 
-var result = Object.keys(obj).map((key) => [String(key), obj[key]]);
-
-console.log(result);
+function toArray(obj) {
+    let result = [];
+    for (let key in obj) {
+      let newArray = [key, obj[key]]
+      result.push(newArray)
+    }
+    return result;
+  }
+  console.log(toArray({a:1,b:2}))
+  console.log(toArray({tesla:12,tats:112}))
 
 
        
